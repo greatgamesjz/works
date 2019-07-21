a982b0cffa737d8059cf29086a02d4df8494996a
Darek <Darek@DESKTOP-9AMPUV2>
2019-07-22 00:25:56 +0200

    Done

---
+++ 4. Existing powers of 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace _4._Existing_powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList decomposedNums = new ArrayList();
+            string input;
+            uint num;
+            while((input = Console.ReadLine())!="")
+            {
+                if (UInt32.TryParse(input, out num))
+                {
+                    decomposeNum(num, decomposedNums);
+                }
+                else Console.WriteLine("NaN");
+            }
+            decomposedNums.Sort();
+            foreach(uint number in decomposedNums)
+            {
+                Console.Write(number+" ");
+            }
+        }
+        private static void decomposeNum(uint num,ArrayList decomposedNums)
+        {
+            uint i = 1;
+            while(i<=num)
+            {
+                if ((i & num) != 0)
+                {
+                    if(!decomposedNums.Contains(i))
+                        decomposedNums.Add(i);
+                }
+                i <<= 1;
+            }
+        }
+    }
+}
