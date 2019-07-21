86eb5bd53180455c49fa51484d24445ec90b7a8a
Darek <Darek@DESKTOP-9AMPUV2>
2019-07-21 19:30:00 +0200

    Done

--- 2. Palindrom/Program.cs
+++ 2. Palindrom/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace _2._Palindrom
+namespace Palindrom
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Regex filtr = new Regex("[^a-zA-Z]");
+            string word = Console.ReadLine();
+            word = filtr.Replace(word, "");
+            bool success = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    success = false;
+                    break;
+                }
+            }
+            if (success)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
         }
     }
 }
