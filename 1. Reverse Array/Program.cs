d6b823c942ca848bbb3c7ee8cab12b16ea03219a
Darek <Darek@DESKTOP-9AMPUV2>
2019-07-21 20:13:32 +0200

    Done

---
+++ 1. Reverse Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] arr;
+            int num;
+            bool success;
+            do
+            {
+                success = Int32.TryParse(Console.ReadLine(), out n);
+                if (!success)
+                    Console.WriteLine("NaN");
+            } while (!success);
+            arr = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                success = Int32.TryParse(Console.ReadLine(), out num);
+                if (success)
+                    arr[i] = num;
+                else
+                {
+                    i--;
+                    Console.WriteLine("NaN");
+                }
+            }
+            for (int i = n - 1; i >= 0; i--)
+                Console.Write(arr[i] + " ");
+                
+        }
+    }
+}
