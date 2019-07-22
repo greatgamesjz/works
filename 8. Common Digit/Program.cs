2e9a8ab2649eb6c15b6ff062f73b20e6ae1d23cc
Darek <Darek@DESKTOP-9AMPUV2>
2019-07-22 09:51:50 +0200

    Done

--- 8. Common Digit/Program.cs
+++ 8. Common Digit/Program.cs
@@ -8,19 +8,19 @@ namespace _8._Common_Digit
         {
             int n;
             int maxIndex = 0, maxValue = 0;
-            int[] digitSum = new int[10];
+            int[] digitCount = new int[10];
             for (int i = 0; i < 10; i++)
-                digitSum[i] = 0;
+                digitCount[i] = 0;
             if (Int32.TryParse(Console.ReadLine(), out n)) ;
             string digits = Console.ReadLine();
             for (int i = 0; i < digits.Length; i++)
                 if (digits[i] >= '0' && digits[i] <= '9')
-                    digitSum[digits[i] - '0']++;
+                    digitCount[digits[i] - '0']++;
             for (int i = 9; i >= 0; i--)
-                if (digitSum[i] > maxValue)
+                if (digitCount[i] > maxValue)
                 {
                     maxIndex = i;
-                    maxValue = digitSum[i];
+                    maxValue = digitCount[i];
                 }
             Console.WriteLine(maxIndex);
 
